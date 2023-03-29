9781c0efe355c7ff7547f80d9c17a36877f2a6b5
Ken Robinson <kenrobinsonter@gmail.com>
2023-03-29 12:41:33 +0000

    Basic params file used in donkey_car_node

--- src/donkey_car/launch/donkey_car.launch.py
+++ src/donkey_car/launch/donkey_car.launch.py
@@ -1,13 +1,20 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    config_file = os.path.join(
+        get_package_share_directory('donkey_car'), 
+        'params', 
+        'donkey_car_config.yaml')
+
     return LaunchDescription([
         Node(
             package='donkey_car',
             namespace='donkey_car',
             executable='donkey_car_node',
-            name='donkey_car'
+            name='donkey_car',
+            parameters=[config_file]
         )
     ])
