b9fef4e362ba03130be64305b6a7d3b73a20781d
Ken Robinson <kenrobinsonter@gmail.com>
2023-05-24 12:41:24 +0000

    Using humble and 22.04

--- src/data_capture/data_capture/image_and_control.py
+++ src/data_capture/data_capture/image_and_control.py
@@ -1,72 +1,62 @@
-from data_capture.data_constants import *
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
-from sensor_msgs.msg import TimeReference
 from tai_interface.msg import VehicleControl
 from waypoint_interfaces.srv import TerminateWaypointLogging, ToggleWaypointLogging, GetWaypointLogState
-from rclpy.qos import QoSPresetProfiles
 from rclpy.qos import qos_profile_sensor_data
-from rclpy.time import Time
 from message_filters import ApproximateTimeSynchronizer
 from message_filters import Subscriber
-import rosbag2_py
-from rclpy.serialization import serialize_message
 
+from typing import Final
+
+IMAGE_TOPIC: Final[str] = '/cam/front'
+CONTROL_TOPIC: Final[str] = '/vehicle_cmd'
 
 class ImageAndControlLogging(Node):
     def __init__(self):
-        # Initialize Node
         super().__init__("image_and_control")
-        self._logger.info("started image and control")
-        # Open a file to write the messages to
-        self.writer = rosbag2_py.SequentialWriter()
-        storage_options = rosbag2_py._storage.StorageOptions(
-            uri=STORAGE_OPTIONS_URI,
-            storage_id=STORAGE_OPTIONS_ID)
-        converter_options = rosbag2_py._storage.ConverterOptions(
-            CONVERTOR_OPTIONS_PARAM_1, CONVERTOR_OPTIONS_PARAM_2)
-        self.writer.open(storage_options, converter_options)
-
-        image_topic_info = rosbag2_py._storage.TopicMetadata(
-            name=IMAGE_TOPIC,
-            type=IMAGE_TOPIC_MSG_TYPE,
-            serialization_format='cdr')
-        self.writer.create_topic(image_topic_info)
-
-        control_topic_info = rosbag2_py._storage.TopicMetadata(
-            name=CONTROL_TOPIC,
-            type=CONTROL_TOPIC_MSG_TYPE,
-            serialization_format='cdr')
-        self.writer.create_topic(control_topic_info)
-
-        # Keep the last image and control
+        self.declare_parameter('image_and_control_logging_hz', 20)
         self.present_image = None
         self.present_control = None
+        
+        self.image_sub = Subscriber(self, Image, IMAGE_TOPIC, qos_profile_sensor_data)
+        self.control_sub = Subscriber(self, VehicleControl, qos_profile_sensor_data)
+        self.tss = ApproximateTimeSynchronizer([self.image_sub, self.control_sub], queue_size=10, slop=0.5)
+        self.tss.registerCallback(self.print_topics)
+        # Pubs N' Subs
+        #self.odom_sub_ = self.create_subscription(
+        #    Odometry, "odom", self.odom_callback_, qos_profile_sensor_data)
+        #self.joy_sub_ = self.create_subscription(
+        #    Joy, "/joy", self.joy_callback_, 1)
 
-        # Subscribe to images and vehicle control to be saved so we can learn from them.
-        self.image_sub = Subscriber(
-            self, Image, IMAGE_TOPIC, qos_profile=qos_profile_sensor_data)
-        self.control_sub = Subscriber(
-            self, VehicleControl, CONTROL_TOPIC, qos_profile=qos_profile_sensor_data)
-        self.tss = ApproximateTimeSynchronizer(
-            [self.image_sub, self.control_sub], queue_size=10, slop=0.001)
-        self.tss.registerCallback(self.write_topics)
+        # ROS parameters
+        #self.default_file_name_ = self.get_parameter(
+        #    "default_save_file_name").get_parameter_value().string_value
+        #self.js_toggle_log_button_ = self.get_parameter(
+        #    "js_toggle_log_button").get_parameter_value().integer_value
+        #self.js_terminate_log_button = self.get_parameter(
+        #    "js_terminate_log_button").get_parameter_value().integer_value
+        #self.js_get_log_state_button = self.get_parameter(
+        #    "js_get_log_state_button").get_parameter_value().integer_value
 
-    def write_topics(self, image: Image, vehicle_control: VehicleControl):
-        self._logger.info("received image " + str(type(image)) +
-                          ", received control " + str(type(vehicle_control)))
+        # ROS services
+        #self.toggle_log_srv_ = self.create_service(ToggleWaypointLogging, "/waypoint/toggle_log",
+        #                                           self.toggle_log_callback_)
+        #self.terminate_log_srv_ = self.create_service(TerminateWaypointLogging, "/waypoint/terminate_log",
+        #                                              self.terminate_log_callback_)
+        #self.get_state_srv_ = self.create_service(GetWaypointLogState, "/waypoint/get_log_state",
+        #                                          self.get_state_callback_)
 
-        time: Time = self.get_clock().now()
+        # Class members
+        #self.state_toggle_log_button_ = 0
+        #self.state_terminate_log_button_ = 0
+        #self.state_get_log_state_button_ = 0
 
-        self.writer.write(
-            IMAGE_TOPIC,
-            serialize_message(image),
-            time.nanoseconds)
-        self.writer.write(
-            CONTROL_TOPIC,
-            serialize_message(vehicle_control),
-            time.nanoseconds)
+    def print_topics(self, image: Image, control: VehicleControl):
+        self.present_image = image
+        self.present_control = control
+        print("New images have been collected.")
+
 
 
 def main(args=None):
@@ -75,7 +65,6 @@ def main(args=None):
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     try:
