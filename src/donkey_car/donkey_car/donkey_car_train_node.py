b59c113f613e498898e0bef99694536163642315
Ken Robinson <kenrobinsonter@gmail.com>
2023-06-26 07:57:55 +0000

    Added Adapter and config params.

--- src/donkey_car/donkey_car/donkey_car_train_node.py
+++ src/donkey_car/donkey_car/donkey_car_train_node.py
@@ -17,6 +17,20 @@ from typing import Final
 from cv_bridge import CvBridge
 from donkeycar.management.base import Train
 
+class DonkeyCarConfigAdapter():
+    def __init__(self, 
+                 default_model_type, 
+                 batch_size, 
+                 image_depth, 
+                 image_h, 
+                 image_w):
+        self.DEFAULT_MODEL_TYPE = default_model_type
+        self.BATCH_SIZE = batch_size
+        self.IMAGE_DEPTH = image_depth
+        self.IMAGE_H = image_h
+        self.IMAGE_W = image_w
+        self.VERBOSE_TRAIN = True
+
 
 class DonkeyCarTrainNode(Node):
     def __init__(self):
@@ -46,13 +60,28 @@ class DonkeyCarTrainNode(Node):
         checkpoint = self.get_parameter('checkpoint')
         transfer_model = self.get_parameter('transfer')
         comment_dbase = self.get_parameter('comment')
-        self._logger.info(tub_data.get_parameter_value().string_value)
 
         framework_str = framework.get_parameter_value().string_value
+        model_type_str = model_type.get_parameter_value().string_value
+        batch_size_int = 128
+        image_depth_int = 3
+        image_width_int = 320
+        image_height_int = 240
+        tub_data_path_str = tub_data.get_parameter_value().string_value
+        output_model_name_str = output_model_name.get_parameter_value().string_value
+        model_type_str = model_type.get_parameter_value().string_value
+        checkpoint_path_str = checkpoint.get_parameter_value().string_value
+
+
+        cfg = DonkeyCarConfigAdapter(model_type_str, 
+                                    batch_size_int, 
+                                    image_depth_int,
+                                    image_width_int,
+                                    image_height_int)
+
         if framework_str == 'pytorch':
             from donkeycar.parts.pytorch.torch_train import train
-            train(cfg, args.tub, args.model, args.type,
-                  checkpoint_path=args.checkpoint)
+            train(cfg, tub_data_path_str, output_model_name_str, model_type_str)
         else:
             self._logger.error(f"Unrecognized framework: {framework}. Please specify "
                                f"one of 'tensorflow' or 'pytorch'")
